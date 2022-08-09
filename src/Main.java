6530ec441b98133dc6b7602178668413b329adb1
maks <mamonov.maksim.20003@gmail.com>
2022-08-09 12:20:23 +0300

    ервое задание выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!"); {
+            //Первое задание
+            for (int i = 1; i <= 10; i++) {
+                System.out.print(i + " ");
+            }
+            System.out.print("\n");
+            for (int s = 10; s > 0; s--) {
+                System.out.print(s + " ");
+
+            }
+
+    }
     }
 }
