031704913603767228e5b1f8d949b10cca15c500
maks <mamonov.maksim.20003@gmail.com>
2022-08-09 13:56:19 +0300

    Третье задание выполнено

--- src/Main.java
+++ src/Main.java
@@ -12,10 +12,19 @@ public class Main {
 
             }
     }
-    //Второе зажание
+    //Второе задание
         int friday = 5;
         for (; friday <= 31; friday = friday + 5) {
             System.out.println("сегодня птница, " + friday + "-ое число. Необходимо одготовить отчёт.");
         }
+    //Третье задание
+    int year = 2022;
+    int year_200 = year - 200;
+    int year100 = year + 100;
+    for (int beginning = 0; beginning <= 2100; beginning = beginning + 79) {
+        if (beginning > year_200 && beginning < year100) {
+            System.out.println(beginning);
+        }
+    }
     }
 }
